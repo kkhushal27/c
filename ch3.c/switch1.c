fcabec244a93aa07dabb8a3d11379af135903bf9
Khushal Singh Bisht <bishtkhushal007@gmail.com>
2026-08-02 16:41:01 +0530

    file was made without entering the particular code

--- ch3.c/switch1.c
+++ ch3.c/switch1.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main (){
+    int age = 2;
+    switch(age){
+        case 1:
+            printf("the age is 1");
+            break;
+        case 2:
+            printf("the age is 2");
+            break;
+        case 3:
+            printf("the age is 3");
+            break;
+        default:
+            printf("the age is not between 1 and 3");
+    }
+    return 0;
+}
