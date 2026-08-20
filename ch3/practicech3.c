7a734237c6e54247b318ed9952817367c4505a4c
kkhushal27 <bishtkhushal007@gmail.com>
2026-08-20 21:06:44 +0530

    added new file

---
+++ ch3/practicech3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    // code to take input from user and tell whether student passed or fail 
+    int marks1, marks2, marks3;
+
+    printf("Enter marks1 \n");
+    scanf("%d", &marks1);
+     printf("Enter marks2 \n");
+    scanf("%d", &marks2 );
+     printf("Enter marks3 \n");
+    scanf("%d", &marks3);
+    
+if (marks1, marks2, marks3 >=33){
+    printf("you are failed");
+}
+    else {
+        printf("you are pass");
+    }
+    return 0;
+}
